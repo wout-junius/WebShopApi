1cfc5950f6cec19066c0e26634f1de207506040e
wout <woutyboy555@gmail.com>
2022-07-22 17:30:45 +0200

    Try fix Cors error

--- src/main/java/com/ehb/webshopapi/filter/CustomAuthentiacationFilter.java
+++ src/main/java/com/ehb/webshopapi/filter/CustomAuthentiacationFilter.java
@@ -2,6 +2,7 @@ package com.ehb.webshopapi.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,7 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 public class CustomAuthentiacationFilter extends UsernamePasswordAuthenticationFilter {
@@ -30,7 +35,9 @@ public class CustomAuthentiacationFilter extends UsernamePasswordAuthenticationF
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        log.info(password, username);
+
+        log.info(username);
+        log.info(password);
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         return authenticationManager.authenticate(authenticationToken);
     }
@@ -52,7 +59,13 @@ public class CustomAuthentiacationFilter extends UsernamePasswordAuthenticationF
                 .withExpiresAt(new Date(System.currentTimeMillis() + 5 * 60 * 60 * 1000) )
                 .sign(algorithm);
 
-        response.setHeader("acces_token", acces_token);
-        response.setHeader("refresh_token", refresh_token);
+//        response.setHeader("acces_token", acces_token);
+//        response.setHeader("refresh_token", refresh_token);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("acces_token", acces_token);
+        tokens.put("refresh_token", refresh_token);
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
